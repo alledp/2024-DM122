e157f7d78c6c5109b9c352c95ef5333430dfcfdf
alledpaiva <alled@freedomfi.com>
2024-07-23 23:09:12 -0300

    feat: requesting the ending point.

--- Web-Storage/cache-api/app.js
+++ Web-Storage/cache-api/app.js
@@ -4,4 +4,16 @@ button.addEventListener("click", (event) => {
     //console.log(event);
     //console.log("👀 Fetch Data Clicked");
     console.count("👀 Button Clicked");
-})
+    console.log(fetchData());
+    console.log("🤔 Wait, Was I called first ?");
+
+});
+
+
+async function fetchData(){
+
+    const endpoint = 'https://swapi.dev/api/people/1';
+    const response = await fetch(endpoint);
+    const character = await response.json();
+    console.log(character);
+}
