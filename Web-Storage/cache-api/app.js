50d14e5b0c93efe7fde882873014acff4544c5bb
alledpaiva <alled@freedomfi.com>
2024-07-23 22:01:49 -0300

    Chore: Added Index and javascript code.
    Using pico CSS and testing JS event.

---
+++ Web-Storage/cache-api/app.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("fetchData");
+
+button.addEventListener("click", (event) => {
+    //console.log(event);
+    //console.log("👀 Fetch Data Clicked");
+    console.count("👀 Button Clicked");
+})
