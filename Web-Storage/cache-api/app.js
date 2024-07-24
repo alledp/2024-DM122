b61ab2558fcabfb689125b3b403712fe345efa6f
alledpaiva <alled@freedomfi.com>
2024-07-24 15:37:40 -0300

    feat: Changing from Starwars to Pokemon

--- Web-Storage/cache-api/app.js
+++ Web-Storage/cache-api/app.js
@@ -2,16 +2,15 @@ const button = document.getElementById("fetchData");
 const header = document.querySelector('header');
 const output = document.querySelector('output');
 
-button.addEventListener("click", (event) => {
+button.addEventListener("click", async() => {
     //console.log(event);
     //console.log("👀 Fetch Data Clicked");
     console.count("👀 Button Clicked");
     //console.log(fetchData());
     setLoadStatus();
-    const character = fetchCharacterData({charId: 1});
-    showCharacterData(character);
+    const poke = await fetchPokeData({pokeId: 1});
+    showCharacterData(poke);
     console.log("✅ Finished");
-
 });
 
 function setLoadStatus(){
@@ -19,17 +18,16 @@ function setLoadStatus(){
 }
 
 function showCharacterData(character){
+    console.log("Chamou a funcao SHOW CHARACTER " + character.name);
     header.textContent = character.name;
     output.textContent = JSON.stringify(character, null, 2);
 }
 
-
-async function fetchCharacterData({ charId }){
-
-    const endpoint = `https://swapi.dev/api/people/${charId}`;
-    console.log(`[fetchCharactedData] #${charId}`);
+async function fetchPokeData({ pokeId }){
+    const endpoint = `https://pokeapi.co/api/v2/pokemon/${pokeId}`;
+    console.log(`[fetchCharactedData] #${pokeId}`);
     const response = await fetch(endpoint);
-    const character = await response.json();
-    console.log(`Character name (${charId}): ${character.name}`);
-    return character;
+    const pokemon = await response.json();
+    console.log(`Character name (${pokeId}): ${pokemon.name}`);
+    return pokemon;
 }
