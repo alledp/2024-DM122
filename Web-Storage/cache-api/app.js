aea33d300dc50b650c5a664975551fbedd85492f
alledpaiva <alled@freedomfi.com>
2024-07-23 23:30:55 -0300

    feat: Multiple requests in parallel.

--- Web-Storage/cache-api/app.js
+++ Web-Storage/cache-api/app.js
@@ -4,16 +4,24 @@ button.addEventListener("click", (event) => {
     //console.log(event);
     //console.log("👀 Fetch Data Clicked");
     console.count("👀 Button Clicked");
-    console.log(fetchData());
-    console.log("🤔 Wait, Was I called first ?");
+    //console.log(fetchData());
+    fetchCharacterData({charId: 1});
+    fetchCharacterData({charId: 2});
+    fetchCharacterData({charId: 3});
+    fetchCharacterData({charId: 4});
+    fetchCharacterData({charId: 5});
+    fetchCharacterData({charId: 6});
+    console.log("✅ Finished");
 
 });
 
 
-async function fetchData(){
+async function fetchCharacterData({ charId }){
 
-    const endpoint = 'https://swapi.dev/api/people/1';
+    const endpoint = `https://swapi.dev/api/people/${charId}`;
+    console.log(`[fetchCharactedData] #${charId}`);
     const response = await fetch(endpoint);
     const character = await response.json();
-    console.log(character);
+    console.log(`Character name (${charId}): ${character.name}`);
+    //console.log(character);
 }
