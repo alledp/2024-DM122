48e9c4258b823b26abd6fd54a8e50c9928a18dfe
alledpaiva <alled@freedomfi.com>
2024-07-30 17:34:35 -0300

    feat: trying dexie.js

---
+++ Web-Storage/indexedDB/cep/app.js
@@ -0,0 +1,18 @@
+import Dexie from 'https://cdn.jsdelivr.net/npm/dexie@4.0.8/+esm';
+
+const response = await fetch(`https://viacep.com.br/ws/37537144/json/`);
+const data = await response.json();
+console.log('response data: ', data);
+
+const db = new Dexie('zipCodeDatabase');
+db.version(1).stores({
+    zipCode: '++id,cep',
+});
+
+db.zipCode.add({
+    zipCode: data.cep.replace("-", ""),
+    uf: data.uf,
+    location: data.localidade,
+    neighborhood: data.bairro,
+    phoneCode: data.ddd,
+});
