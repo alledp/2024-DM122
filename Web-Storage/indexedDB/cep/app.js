6eb7bf44b09c5de8a30a677be6bf116dc28c1101
alledpaiva <alled@freedomfi.com>
2024-08-05 23:15:32 -0300

    feat: Load CSV to extract CEPs

--- Web-Storage/indexedDB/cep/app.js
+++ Web-Storage/indexedDB/cep/app.js
@@ -1,19 +1,36 @@
 import Dexie from 'https://cdn.jsdelivr.net/npm/dexie@4.0.8/+esm';
 
-const response = await fetch(`https://viacep.com.br/ws/37537144/json/`);
-const data = await response.json();
-console.log('response data: ', data);
+async function extractCEPsOnly(){
+    const response = await fetch('./CEPs.txt');
+    const textData = await response.text();
+    const [firstLine] = textData.split('\n');
+    const [,,,cep] = firstLine.split(';');
+    console.log(firstLine);
+    console.log(cep);
+}
 
-const db = new Dexie('zipCodeDatabase');
-db.version(1).stores({
-    zipCode: '++id,cep',
-});
+console.time('Extracting CEP');
+extractCEPsOnly();
+console.timeEnd('Extracting CEP');
 
-db.zipCode.add({
-    zipCode: data.cep.replace("-", ""),
-    uf: data.uf,
-    location: data.localidade,
-    publicPlace: data.logradouro,
-    neighborhood: data.bairro,
-    phoneCode: data.ddd,
-});
+async function fetchCEPData (cep) {
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const data = await response.json();
+    console.log('response data: ', data);
+    return data;
+}
+
+// const db = new Dexie('zipCodeDatabase');
+
+// db.version(1).stores({
+//     zipCode: '++id,cep',
+// });
+
+// db.zipCode.add({
+//     zipCode: data.cep.replace("-", ""),
+//     uf: data.uf,
+//     location: data.localidade,
+//     publicPlace: data.logradouro,
+//     neighborhood: data.bairro,
+//     phoneCode: data.ddd,
+// });
