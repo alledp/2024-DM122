1d85a3b31709ebcc6dc53cad336308ce985d9ee9
alledpaiva <alled@freedomfi.com>
2024-08-11 20:18:10 -0300

    feat: fetch Button Added

--- Workout-Control/app.js
+++ Workout-Control/app.js
@@ -1,10 +1,12 @@
 const form = document.querySelector('form');
+
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
     console.log('form submitted');
     const obj = new Object();
     obj.grupoMuscular = form.grupoMuscular.value;
     obj.exercicio = form.exercicio.value;
+    obj.series = form.series.value;
     obj.repeticoes = form.repeticoes.value;
     obj.peso = form.peso.value;
 
@@ -17,5 +19,11 @@ form.addEventListener('submit', async (event) => {
     });
 
     await db.workout.add(obj).then((result) => (console.log('DataSaved!',result)));
-     
+});
+
+const button = document.getElementById('searchButton');
+
+button.addEventListener('click', async (event) => {
+    const group = document.getElementById("selectGroup");
+    console.log(`BUSCAR: ${group.options[group.selectedIndex].text}`);
 });
